2284b653207cd9f5afa224c35ff2a9f35f50053f
DannyGilligan <gilligan.danny@gmail.com>
2023-05-29 19:55:25 +0000

    Add Tracker class in models.py

--- tracker/models.py
+++ tracker/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Tracker(models.Model):
+    status = models.CharField(max_length= 6, null=False, blank=False)
+    ticker = models.CharField(max_length= 8, null=False, blank=False) 
+    position = models.CharField(max_length= 4, null=False, blank=False)
+    date_opened = models.DateField(null=False, blank=False) 
+    amount_traded = models.DecimalField(max_digits=8, decimal_places=2, null=False, blank=False) 
+    opening_price = models.DecimalField(max_digits=8, decimal_places=2, null=False, blank=False)
+    closing_price = models.DecimalField(max_digits=8, decimal_places=2)
+    date_closed = models.DateField() 
+    profit_loss = models.DecimalField(max_digits=8, decimal_places=2)
+    result = models.CharField(max_length= 4) 
