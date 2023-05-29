1604cdc6a99b4f788cfb421ec3035822f864b30a
DannyGilligan <gilligan.danny@gmail.com>
2023-05-29 18:49:36 +0000

    Add get_tracker function to views.py

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, 
 
 # Create your views here.
 
-    
+def get_tracker(request):
+    return render(request, "tracker/tracker.html")
