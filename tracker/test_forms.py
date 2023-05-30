ffcaefe581b194dc0c7a6cd65aa646d467238b41
DannyGilligan <gilligan.danny@gmail.com>
2023-05-30 17:12:48 +0000

    Add automated tests for required TradeForm fields in test_forms.py

--- tracker/test_forms.py
+++ tracker/test_forms.py
@@ -1,9 +1,48 @@
 from django.test import TestCase
+from .forms import TradeForm
 
 # Create your tests here.
 
+#This will test the required fields that are needed on the TradeForm in order to add a trade to the tracker. The code for this was taken from the Code Institute LMS (testing forms section)
+
 class TestDjango(TestCase):
 
-    def test_this_thing_works1(self):
-        self.assertEqual(1,1)
+    def test_trade_status_required(self):
+        form = TradeForm({'status': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('status', form.errors.keys())
+        self.assertEqual(form.errors['status'][0], 'This field is required.')
+
+    def test_trade_ticker_required(self):
+        form = TradeForm({'ticker': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('ticker', form.errors.keys())
+        self.assertEqual(form.errors['ticker'][0], 'This field is required.')
+
+    def test_trade_position_required(self):
+        form = TradeForm({'position': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('position', form.errors.keys())
+        self.assertEqual(form.errors['position'][0], 'This field is required.')
+
+    def test_trade_date_opened_required(self):
+        form = TradeForm({'date_opened': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('date_opened', form.errors.keys())
+        self.assertEqual(form.errors['date_opened'][0], 'This field is required.')
+
+    def test_trade_amount_traded_required(self):
+        form = TradeForm({'amount_traded': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('amount_traded', form.errors.keys())
+        self.assertEqual(form.errors['amount_traded'][0], 'This field is required.')
+
+    def test_trade_opening_price_required(self):
+        form = TradeForm({'opening_price': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('opening_price', form.errors.keys())
+        self.assertEqual(form.errors['opening_price'][0], 'This field is required.')
+
+        
+
 
