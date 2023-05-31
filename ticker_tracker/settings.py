500c2e1b0d89d00c455f7b72d14e31d8b649cd4b
DannyGilligan <gilligan.danny@gmail.com>
2023-05-31 18:24:12 +0000

    Update SECRET_KEY

--- ticker_tracker/settings.py
+++ ticker_tracker/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-&1412z#e#5b7)57o(sp$k!6kvl=%a_3^%ne52p@ycb@3cz2f6*')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
