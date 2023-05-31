5d253f99227df135a6644189c21f1f8162210886
DannyGilligan <gilligan.danny@gmail.com>
2023-05-31 08:27:49 +0000

    Set up environment variables

--- ticker_tracker/settings.py
+++ ticker_tracker/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&1412z#e#5b7)57o(sp$k!6kvl=%a_3^%ne52p@ycb@3cz2f6*'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-&1412z#e#5b7)57o(sp$k!6kvl=%a_3^%ne52p@ycb@3cz2f6*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ticker-tracker.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
